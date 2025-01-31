ed14f56751e30e9b107cf721cce335c751582f6c
Eug1n1 <vkzheny@gmail.com>
2025-01-31 21:37:48 +0300

    added ansible filetype pattern

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("config")
 require("autocmds")
+
+vim.filetype.add({
+    pattern = {
+        ['.*/playbooks/.*'] = 'yaml.ansible',
+        ['.*/docker-compose.yaml'] = 'yaml.docker-compose',
+    },
+})
