eb59e64b069f8f0985859f55ffb96134b582b41c
Shumskiy Evgeniy <vkzheny@gmail.com>
2026-02-06 11:29:48 +0300

    turn diagnostic on
    Signed-off-by: Shumskiy Evgeniy <vkzheny@gmail.com>

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,4 +3,6 @@ require("config.filetypes")
 require("config.lsp")
 require("config.keymaps")
 require("config.colorscheme")
+require("config.diagnostic")
 require("config.trailing-spaces")
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,10 @@ vim.keymap.set('n', '<leader>e', ':Oil --float<CR>')
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+
 --- Navigate buffers bypassing the menu
 local bmui = require("buffer_manager.ui")
 
